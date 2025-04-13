293ab12c91d6d7a943181e3bb502bb9bd8e0a662
Pablo Silva <pablos.epost@gmail.com>
2025-04-13 15:29:24 +0200

    feat: add new deck logic to controller and show remaining cards in templates
    - Implemented the new card draw logic in the controller
    - Display remaining number of cards in the twig template

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -2,7 +2,8 @@
 
 namespace App\Controller;
 
-use App\Card\Card;
+// use App\Card\Card;
+use App\Card\DeckOfCards;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -10,13 +11,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CardGameController extends AbstractController
 {
-    private Card $card;
-
-    public function __construct(Card $card)
-    {
-        $this->card = $card;
-    }
-
     #[Route("/card", name: "card_start")]
     public function home(): Response
     {
@@ -27,12 +21,13 @@ class CardGameController extends AbstractController
         return $this->render('card/home.html.twig', $data);
     }
 
-
     #[Route("/card/deck", name: "card_deck")]
     public function deck(SessionInterface $session): Response
     {
         if (!$session->has("deck")) {
-            $deck = $this->card->deck();
+            $cards = new DeckOfCards();
+            $deck = $cards->getDeck();
+            $session->set('deck', $cards);
         } else {
             $deck = $session->get("deck");
         }
@@ -45,76 +40,80 @@ class CardGameController extends AbstractController
         return $this->render('card/deck.html.twig', $data);
     }
 
-
     #[Route("/card/deck/shuffle", name: "deck_shuffle")]
     public function deckShuffle(SessionInterface $session): Response
     {
 
-        $shuffleDeck = $this->card->shuffleDeck();
+        $cards = new DeckOfCards();
+        $shuffleDeck = $cards->shuffle();
+        $deck = $cards->getDeck();
 
-        $session->set('deck', $shuffleDeck);
+        $session->set('deck', $cards);
 
         $data = [
             'name' => 'Card Deck',
-            'deck' => $shuffleDeck,
+            'deck' => $deck,
         ];
 
         return $this->render('card/deck.html.twig', $data);
     }
-
 
     #[Route("/card/deck/draw", name: "card_draw")]
     public function deckDraw(SessionInterface $session): Response
     {
 
         if (!$session->has("deck")) {
-            $drawCard = $this->card->drawCard();
+            $cards = new DeckOfCards();
+            $deck = $cards->getDeck();
         } else {
-            $drawData = $session->get("deck");
-            $deck = $drawData['deck']; 
-            $drawCard = $this->card->drawCard(1, $deck);
+            $deck = $session->get("deck");
+
+            $drawCard = $deck->draw();
 
             if ($drawCard === null) {
-                $session->set('deck', $drawCard);
+                $session->set('deck', $deck);
                 $this->addFlash('warning', 'No more cards left!');
                 return $this->redirectToRoute('card_deck'); 
             }
         }
-
-        $session->set('deck', $drawCard);
+        
+        $session->set('deck', $deck);
+        $remainingCards = $deck->getRemaining();
 
         $data = [
             'name' => 'Card Draw',
             'card' => $drawCard,
+            'remainingCards' => $remainingCards,
         ];
 
         return $this->render('card/draw.html.twig', $data);
     }
-
 
     #[Route("/card/deck/draw/{number<\d+>}", name: "draw_amount")]
     public function deckDrawMulti(SessionInterface $session, int $number): Response
     {
 
         if (!$session->has("deck")) {
-            $drawCard = $this->card->drawCard($number);
+            $cards = new DeckOfCards();
+            $deck = $cards->getDeck();
         } else {
-            $drawData = $session->get("deck");
-            $deck = $drawData['deck']; 
-            $drawCard = $this->card->drawCard($number, $deck);
+            $deck = $session->get("deck");
+            $drawCard = $deck->draw($number);
 
             if ($drawCard === null) {
-                $session->set('deck', $drawCard);
+                $session->set('deck', $deck);
                 $this->addFlash('warning', 'No more cards left!');
                 return $this->redirectToRoute('card_deck'); 
             }
         }
 
-        $session->set('deck', $drawCard);
+        $session->set('deck', $deck);
+        $remainingCards = $deck->getRemaining();
 
         $data = [
             'name' => 'Card Draw',
             'card' => $drawCard,
+            'remainingCards' => $remainingCards,
         ];
 
         return $this->render('card/draw.html.twig', $data);
