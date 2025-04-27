97d3ee75ac6a96f3e4f5ca488165655fa416ca4d
Pablo Silva <pablos.epost@gmail.com>
2025-04-27 22:29:18 +0200

    Update Card class to return int value for all card types

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -7,6 +7,13 @@ class Card
     private string $value;
     private string $suit;
 
+    private array $representation = [
+        'J' => 11,
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+    ];
+
     public function __construct(string $value, string $suit)
     {
         $this->value = $value;
@@ -16,6 +23,15 @@ class Card
     public function getValue(): string
     {
         return $this->value;
+    }
+
+    public function getIntValue(): int
+    {
+        $value = $this->getValue();
+        if (isset($this->representation[$value])) {
+            return $this->representation[$value];
+        }
+        return intval($value);
     }
 
     public function getSuit(): string
