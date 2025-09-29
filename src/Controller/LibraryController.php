387ab9a5da33bead9482d3e8390601c9b8fb6fe2
Pablo Silva <pablos.epost@gmail.com>
2025-09-29 23:09:24 +0200

    feat(form):wire "Add Book" form to DB (title, author, isbn, cover image)

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 
-use App\Card\DeckOfCards;
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
 
 class LibraryController extends AbstractController
 {
@@ -20,26 +27,62 @@ class LibraryController extends AbstractController
         return $this->render('library/home.html.twig', $data);
     }
 
-    #[Route("/library/add_book", name: "add_book")]
-    public function add_book(): Response
+    #[Route("/library/add_book", name: "add_book", methods: ['GET','POST'])]
+    public function add_book(Request $request, ManagerRegistry $doctrine, ValidatorInterface $validator): Response
     {
+        $entityManager = $doctrine->getManager();
+
+        if ($request->isMethod('POST')) {
+
+            $title  = trim((string) $request->request->get('book-title'));
+            $author = trim((string) $request->request->get('author'));
+            $isbn  = $request->request->get('book-isbn');
+            $coverFile = $request->files->get('file-upload');
+
+            $book = new Book();
+            $book->setTitle($title);
+            $book->setAuthor($author);
+            $book->setIsbn($isbn);
+
+            if ($coverFile instanceof UploadedFile) {
+                $violations = $validator->validate(
+                $coverFile,
+                new \Symfony\Component\Validator\Constraints\Image([
+                    'maxSize' => '5M',
+                    'mimeTypes' => ['image/jpeg', 'image/png'],
+                    'detectCorrupted' => true,
+                ])
+                );
+
+                $mime = $coverFile->getMimeType();
+                $ext  = match ($mime) {
+                'image/jpeg' => 'jpg',
+                'image/png'  => 'png',
+                default      => null,
+                };
+
+                $coverName = bin2hex(random_bytes(8)).'.'.$ext;
+                $coverFile->move($this->getParameter('covers_dir'), $coverName);
+                $book->setCoverUrl('/img/covers/'.$coverName);
+            }
+
+            $entityManager->persist($book);
+            $entityManager->flush();
+
+        }
+
         $data = [
-            'name' => 'Add book'
+            'name' => "Add book"
         ];
 
         return $this->render('library/addBook.html.twig', $data);
     }
 
     #[Route("/library/books", name: "library_books")]
-    public function books(): Response
+    public function books(BookRepository $bookRepository): Response
     {
-
-        $books = [
-            ['id'=>1,'title'=>'Sagan om ringen','author'=>'J.R.R. Tolkien','coverUrl'=> 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-01.jpg'],
-            ['id'=>2,'title'=>'Clean Code','author'=>'Robert C. Martin','coverUrl'=> 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-01.jpg'],
-            ['id'=>3,'title'=>'Atomic Habits','author'=>'James Clear'],
-        ];
-
+        $books = $bookRepository
+            ->findALL();
 
         $data = [
             'name' => 'Books',
