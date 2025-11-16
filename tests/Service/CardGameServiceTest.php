3294322a458a277be6edfaf2b1a29a2cf7a7e6de
Pablo Silva <pablos.epost@gmail.com>
2025-11-16 16:04:43 +0100

    test: add setup for CardGameService tests

---
+++ tests/Service/CardGameServiceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\tests\Service;
+use App\Service\CardGameService;
+use App\Card\DeckOfCards;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+
+class CardGameServiceTest extends TestCase
+{
+    private CardGameService $cardGame;
+    /** @phpstan-ignore-next-line */
+    private array $sessionStorage;
+
+
+    protected function setUp(): void
+    {
+        $this->sessionStorage = [];
+        $session = $this->createMock(SessionInterface::class);
+
+        $session->method('get')
+            ->willReturnCallback(function (string $key, $value = null) {
+                return $this->sessionStorage[$key] ?? $value;
+            });
+
+        $session->method('set')
+            ->willReturnCallback(function (string $key, $value = null) {
+                $this->sessionStorage[$key] = $value;
+            });
+
+        $requestStack = $this->createMock(RequestStack::class);
+        $requestStack->method('getSession')->willReturn($session);
+
+        $this->cardGame = new CardGameService($requestStack);
+
+    }
+}
