026a603421a0b4c7d8c301fde34a7331868186d9
Pablo Silva <pablos.epost@gmail.com>
2025-11-20 18:30:09 +0100

    feat(blackjack) add more test for HandTest and changed code after composer lint

--- src/blackjack/Hand.php
+++ src/blackjack/Hand.php
@@ -3,41 +3,72 @@
 namespace App\blackjack;
 
 use App\Card\DeckOfCards;
+use App\Card\CardGraphic;
 use App\Card\Card;
+use UnderflowException;
 
 class Hand
 {
+    /** @var Card[] */
     private array $cards = [];
     private int $bet = 0;
     private bool $isStanding = false;
 
 
+    /**
+     * @param array<int, Card> $cards
+     */
     public function __construct(array $cards, int $bet)
     {
         $this->cards = $cards;
         $this->bet = $bet;
     }
 
-    public function addCard($card): void
+    public function addCard(Card $card): void
     {
         $this->cards[] = $card;
     }
 
+    /**
+     * @return array<int, Card>
+     */
     public function getCards(): array
     {
         return $this->cards;
     }
 
+    public function getBet(): int
+    {
+        return $this->bet;
+    }
+
     public function hit(DeckOfCards $deck): void
     {
-        $card = $deck->draw()[0];
+        $cards = $deck->draw();
 
-        $card->addCard($card);
+        if ($cards === null) {
+            throw new UnderflowException('Deck is empty!');
+        }
+
+        $card = $cards[0];
+
+        $this->addCard($card);
     }
 
     public function stand(): void
     {
         $this->isStanding = true;
+    }
+
+    public function isBusted(): bool
+    {
+        $value = $this->getValue();
+
+        if ($value > 21) {
+            return true;
+        }
+
+        return false;
     }
 
     public function isStanding(): bool
@@ -50,26 +81,27 @@ class Hand
         $value = 0;
         $aces = 0;
 
-        /** @var Card[]  $drawncards */
         foreach ($this->cards as $card) {
             $cardValue = $card->getIntValue();
+            $add = $cardValue;
 
             if ($cardValue === 14) {
                 $aces++;
-                $value += 11;
-            } elseif (in_array($cardValue, ['J', 'Q', 'K'])) {
-                $value += 10;
-            } else {
-                $value += $cardValue;
+                $add = 11;
             }
+
+            if (in_array($cardValue, [11, 12, 13])) {
+                $add = 10;
+            }
+
+            $value += $add;
         }
 
         while ($value > 21 && $aces > 0) {
-                $value -= 10;
-                $aces--;
-            }
+            $value -= 10;
+            $aces--;
+        }
 
         return $value;
     }
-
 }
---
+++ tests/blackjack/HandTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\blackjack;
+
+use App\blackjack\Hand;
+use App\Card\DeckOfCards;
+use App\Card\CardGraphic;
+use App\Card\Card;
+use PHPUnit\Framework\TestCase;
+
+class HandTest extends TestCase
+{
+    private DeckOfCards $deckOfCards;
+
+    /** @var CardGraphic[] */
+    private $cards;
+
+    private Hand $hand;
+
+
+    protected function setUp(): void
+    {
+        $this->deckOfCards = new DeckOfCards();
+        $cards = $this->deckOfCards->draw(2);
+        self::assertNotNull($cards);
+
+        $this->cards = $cards;
+
+        $this->hand = new Hand($this->cards, 100);
+    }
+
+    public function testCreateHand(): void
+    {
+        $this->assertEquals(100, $this->hand->getBet());
+        $this->assertCount(2, $this->hand->getCards());
+    }
+
+
+    public function testAddCardToHand(): void
+    {
+        $newCards = $this->deckOfCards->draw(1);
+        self::assertNotNull($newCards);
+
+        $this->hand->addCard($newCards[0]);
+
+        $this->assertCount(3, $this->hand->getCards());
+    }
+
+    public function testHit(): void
+    {
+        $this->hand->hit($this->deckOfCards);
+        $this->assertCount(3, $this->hand->getCards());
+    }
+
+    public function testStandTrue(): void
+    {
+        $this->hand->stand();
+        $stand = $this->hand->isStanding();
+        $this->assertTrue($stand);
+    }
+
+    public function testGetValue(): void
+    {
+
+        $card1 = new Card('6', 'H');
+        $card2 = new Card('K', 'D');
+
+        $cards = [$card1, $card2];
+
+        $hand = new Hand($cards, 100);
+
+        $this->assertEquals(16, $hand->getValue());
+    }
+
+    public function testGetValueWithAcesAndUnder21(): void
+    {
+
+        $card1 = new Card('6', 'H');
+        $card2 = new Card('A', 'D');
+
+        $cards = [$card1, $card2];
+
+        $hand = new Hand($cards, 100);
+
+        $this->assertEquals(17, $hand->getValue());
+    }
+
+    public function testGetValueWithAcesAndUOver21(): void
+    {
+
+        $card1 = new Card('Q', 'H');
+        $card2 = new Card('A', 'D');
+        $card3 = new Card('K', 'C');
+
+        $cards = [$card1, $card2, $card3];
+
+        $hand = new Hand($cards, 100);
+
+        $this->assertEquals(21, $hand->getValue());
+    }
+
+    public function testIsBusted(): void
+    {
+        $card1 = new Card('6', 'H');
+        $card2 = new Card('K', 'D');
+        $card3 = new Card('K', 'S');
+
+        $cards = [$card1, $card2, $card3];
+
+        $hand = new Hand($cards, 100);
+
+        $this->assertTrue($hand->isBusted());
+    }
+}
